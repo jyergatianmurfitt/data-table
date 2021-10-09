fec10e42f2968b1f0064e41792e9e7f108a1a87a
Josephina <jyergatianmurfitt@googlemail.com>
2021-10-09 12:43:01 +0100

    images added to table

--- src/table.js
+++ src/table.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon, Label, Menu, Table, Image } from 'semantic-ui-react'
 import axios from 'axios';
 
 const url = 'https://mindfuleducation-cdn.s3.eu-west-1.amazonaws.com/misc/data.json';
@@ -16,7 +16,7 @@ const Colleges = () => {
 
   return (
     <div>
-      <Table celled>
+      <Table celled style={{width: "60%"}}>
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>Partner</Table.HeaderCell>
@@ -27,7 +27,7 @@ const Colleges = () => {
         </Table.Header>
 
         <Table.Body>
-          {college.getColleges.map(d => (<Table.Row><Table.Cell>{d.name}</Table.Cell><Table.Cell>{college.getColleges[0].groupPrefix}</Table.Cell><Table.Cell>{college.getColleges[0].logo}</Table.Cell><Table.Cell>{college.getColleges[0].ofstedRating}</Table.Cell></Table.Row>))}
+          {college.getColleges.map(d => (<Table.Row><Table.Cell>{d.name}</Table.Cell><Table.Cell>{d.groupPrefix}</Table.Cell><Table.Cell><Image src={d.logo} size='tiny'/></Table.Cell><Table.Cell>{d.ofstedRating}</Table.Cell></Table.Row>))}
         </Table.Body>
       </Table>
     </div>
