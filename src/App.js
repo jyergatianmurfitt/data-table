8185f90f36ebb63e5e5b16ea62e8c7d18250a29d
Josephina <jyergatianmurfitt@googlemail.com>
2021-10-09 09:08:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TableExamplePagination from './table';
+
+function App() {
+  return (
+    <div className="App">
+      <TableExamplePagination></TableExamplePagination>
+    </div>
+  );
+}
+
+export default App;
