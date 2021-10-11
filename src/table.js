da0777c2ec36baeb4e4c75dfa824d2fe2be75e50
Josephina <jyergatianmurfitt@googlemail.com>
2021-10-11 15:45:04 +0100

    sorting function added

--- src/table.js
+++ src/table.js
@@ -1,49 +1,93 @@
 
+import _ from 'lodash'
 import React from 'react'
-import { Icon, Label, Menu, Table, Image } from 'semantic-ui-react'
+import { Table, Image } from 'semantic-ui-react'
 import axios from 'axios';
 
 const url = 'https://mindfuleducation-cdn.s3.eu-west-1.amazonaws.com/misc/data.json';
 
-const Colleges = () => {
-  const [college, setCollege] = React.useState(null);
+function exampleReducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_SORT':
+      if (state.column === action.column) {
+        return {
+          ...state,
+          data: state.data.slice().reverse(),
+          direction:
+            state.direction === 'ascending' ? 'descending' : 'ascending',
+        }
+      }
 
+      return {
+        column: action.column,
+        data: _.sortBy(state.data, [action.column]),
+        direction: 'ascending',
+      }
+    default:
+      throw new Error()
+  }
+}
+
+const Colleges = () => {
+  let tableData;
+  const [state, dispatch] = React.useReducer(exampleReducer, {
+    column: null,
+    data: tableData,
+    direction: null,
+  })
+  const { column, data, direction } = state
+
+
+  const [college, setCollege] = React.useState(null);
   React.useEffect(() => {
     axios.get(url).then((response) => {
       setCollege(response.data);
     });
   }, []);
   if(!college) return null;
+  tableData = college.getColleges;
+
 
   return (
-    <div>
-      <Table celled style={{width: "60%"}}>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Partner</Table.HeaderCell>
-            <Table.HeaderCell>Prefix</Table.HeaderCell>
-            <Table.HeaderCell>Logo/Preroll</Table.HeaderCell>
-            <Table.HeaderCell>Ofsted Rating</Table.HeaderCell>
+    <Table sortable celled fixed style={{width: "60%"}}>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell
+            sorted={column === 'name' ? direction : null}
+            onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'name' })}>
+            Partner
+          </Table.HeaderCell>
+          <Table.HeaderCell
+            sorted={column === 'Prefix' ? direction : null}
+            onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'Prefix' })}>
+            Prefix
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            Logo/Preroll
+          </Table.HeaderCell>
+          <Table.HeaderCell
+            sorted={column === 'OfstedRating' ? direction : null}
+            onClick={() => dispatch({ type: 'CHANGE_SORT', column: 'OfstedRating' })}>
+            Ofsted Rating
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+      {tableData.map(d => (
+          <Table.Row key={d.name}>
+          <Table.Cell>{d.name}</Table.Cell>
+          <Table.Cell>{d.groupPrefix}</Table.Cell>
+          <Table.Cell><Image src={d.logo} size='tiny' centered/></Table.Cell>
+          {d.ofstedRating === 'Good' || d.ofstedRating === 'Outstanding' ?
+            <Table.Cell positive>{d.ofstedRating}</Table.Cell> :
+            d.ofstedRating === 'Inadequate' ?
+            <Table.Cell negative>{d.ofstedRating}</Table.Cell> :
+            <Table.Cell warning>{d.ofstedRating}</Table.Cell>}
           </Table.Row>
-        </Table.Header>
+        ))}
 
-        <Table.Body>
-          {college.getColleges.map(d => (
-            <Table.Row>
-            <Table.Cell>{d.name}</Table.Cell>
-            <Table.Cell>{d.groupPrefix}</Table.Cell>
-            <Table.Cell><Image src={d.logo} size='tiny' centered/></Table.Cell>
-            {d.ofstedRating == 'Good' || d.ofstedRating == 'Outstanding' ?
-              <Table.Cell positive>{d.ofstedRating}</Table.Cell> :
-              d.ofstedRating == 'Inadequate' ?
-              <Table.Cell negative>{d.ofstedRating}</Table.Cell> :
-              <Table.Cell warning>{d.ofstedRating}</Table.Cell>}
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    </div>
-  );
+      </Table.Body>
+    </Table>
+  )
 }
-
 export default Colleges
